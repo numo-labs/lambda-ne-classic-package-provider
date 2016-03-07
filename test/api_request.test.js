cbc3a44422f4df375448f53c6695f5a6800a3878
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-07 17:39:29 +0000

    an API proxy in 24 lines of code and only node core modules

--- test/api_request.test.js
+++ test/api_request.test.js
@@ -1,9 +1,20 @@
-// var api_request = require('../lib/api_request');
-// var assert = require('assert');
-//
-// describe('api_request', function () {
-//   it('call the nordics classic API', function (done) {
-//     assert.equal(true, true);
-//     done();
-//   });
-// });
+var api_request = require('../lib/api_request');
+var assert = require('assert');
+
+describe('api_request', function () {
+  it('call the nordics classic API', function (done) {
+    var params = {
+      adults: 2,
+      children: 2,
+      allInclusive: 'true'
+    };
+    api_request(params, function (err, json) {
+      assert(!err);
+      console.log(json.result[0]);
+      console.log('Result Count:', json.result.length);
+      assert(json.result.length > 10);
+      assert(json.totalHits > 10);
+      done();
+    });
+  });
+});
