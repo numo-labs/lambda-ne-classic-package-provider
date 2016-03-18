8174561dc802d824c34ec549ce8a199aa5da5dee
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-18 11:46:19 +0000

    add test for stage:$LATEST

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var CONTEXT = {
   functionName: 'LambdaTest',
   functionVersion: '1',
-  invokedFunctionArn: 'arn:aws:lambda:eu-west-1:655240711487:function:LambdaTest:ci'
+  invokedFunctionArn: 'arn:aws:lambda:eu-west-1:655240711487:function:LambdaTest:$LATEST'
 };
 
 var SNS_MESSAGE = {
@@ -26,8 +26,21 @@ console.log(' - - - - - - - - - - - - - - - - - - - - TEST SNS MESSAGE:');
 console.log(JSON.stringify(EVENT, null, 2));
 console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
 
-describe('Search request handler', function () {
+describe('Search request handler $LATEST', function () {
   it('invoke the lambda function handler', function (done) {
+    CONTEXT.succeed = function () {
+      console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
+      // console.log(arguments); // the argument to context.succeed
+      assert(parseInt(arguments[0], 10) > 1);
+      done();
+    };
+    handler(EVENT, CONTEXT);
+  });
+});
+
+describe('Search request handler CI', function () {
+  it('invoke the lambda function handler', function (done) {
+    CONTEXT.invokedFunctionArn = 'arn:aws:lambda:eu-west-1:12345:function:LambdaTest:ci';
     CONTEXT.succeed = function () {
       console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
       // console.log(arguments); // the argument to context.succeed
