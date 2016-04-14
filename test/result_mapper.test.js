4cdf282e76b439afe730dcaed28a180c2f51ba61
nelsonic <contact.nelsonic+github@gmail.com>
2016-04-14 11:29:08 +0100

    update setup for test simulating failure of hotels API

--- test/result_mapper.test.js
+++ test/result_mapper.test.js
@@ -5,7 +5,7 @@ var path = require('path');
 var dir = path.resolve(__dirname + '/sample_results/') + '/';
 // Sample Hotels API Query Result Saved by the api_request.test.js
 var sample_hotels_result_filename = dir + 'NE_hotels_without_trips.json';
-var sample_hotels_result = JSON.parse(fs.readFileSync(sample_hotels_result_filename, 'utf8'));
+var sample_hotels_result = require(sample_hotels_result_filename);
 var sample_packages_result_filename = dir + 'NE_trips_with_hotels.json';
 var sample_packages_result = JSON.parse(fs.readFileSync(sample_packages_result_filename, 'utf8'));
 
@@ -69,11 +69,12 @@ describe('Map results and hotels', function () {
 });
 
 var sample_packages_without_hotels = dir + 'NE_trips_without_hotels.json';
-var sample_packages_result_without = JSON.parse(fs.readFileSync(sample_packages_without_hotels, 'utf8'));
+var sample_packages_result_without = require(sample_packages_without_hotels);
 
 describe('Simulate Failure Where a hotels API does not return hotel detail', function () {
   it('map_ne_result_to_graphql returns early when no hotel details found', function (done) {
-    var result = mapper.map_ne_result_to_graphql(sample_packages_result_without.result, sample_hotels_result.result);
+    var one_hotel = sample_hotels_result.result.slice(0,1); // ensures hotel is not found
+    var result = mapper.map_ne_result_to_graphql(sample_packages_result_without.result, one_hotel);
     // console.log(result);
     assert.equal(result.length, 0);
     done();
