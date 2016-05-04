ad615c5d79958d2fa79a3277a26ff45ec83ce3a1
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-04 11:34:03 +0100

    expect at least one package result.

--- test/index.test.js
+++ test/index.test.js
@@ -21,25 +21,23 @@ console.log(' - - - - - - - - - - - - - - - - - - - - TEST SNS MESSAGE:');
 console.log(JSON.stringify(EVENT, null, 2));
 console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
 
-describe('Search request handler $LATEST', function () {
-  it('invoke the lambda function handler', function (done) {
+describe('Search request handler ', function () {
+  it('invoke the lambda function handler $LATEST', function (done) {
     CONTEXT.succeed = function () {
       console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
       console.log(arguments); // the argument to context.succeed
-      assert(parseInt(arguments[0], 10) > 1);
+      assert(parseInt(arguments[0], 10) > 0);
       done();
     };
     handler(EVENT, CONTEXT);
   });
-});
 
-describe('Search request handler CI', function () {
-  it('invoke the lambda function handler', function (done) {
+  it('invoke the lambda function handler CI', function (done) {
     CONTEXT.invokedFunctionArn = 'arn:aws:lambda:eu-west-1:12345:function:LambdaTest:ci';
     CONTEXT.succeed = function () {
       console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
-      // console.log(arguments); // the argument to context.succeed
-      assert(parseInt(arguments[0], 10) > 1);
+      console.log(arguments); // the argument to context.succeed
+      assert(parseInt(arguments[0], 10) > 0);
       done();
     };
     handler(EVENT, CONTEXT);
