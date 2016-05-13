1333fce19cd8ee38cfe606499cffcae4493d2f2f
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-13 14:46:55 +0100

    disable the on-lambda cache of complete results

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var unique_packages = require('./lib/unique_packages');
 var mapper = require('./lib/result_mapper');
 var batch_insert = require('./lib/dynamo_insert');
 var parse_sns = require('./lib/parse_sns');
-var CACHE = {};
 /**
  * handler receives an SNS message with search parameters and makes requests
  * to the ThomasCook Nordics "Classsic" Packages API. Once we get results
@@ -24,43 +23,30 @@ exports.handler = function (event, context) {
   var bucketId = params.id; // we need the bucketId to insert the results
   delete params.id;         // don't send bucketId to NE api
 
-  var path = api_request.make_path_from_params(params);
-  console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
-  console.log('PATH:', path);
-  console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
-  if (CACHE[path]) {
-    var records = CACHE[path];
-    console.log('CACHE HIT:', records.length + ' records');
-    batch_insert(stage, bucketId, records, function (err, data) {
-      AwsHelper.log.info({ err: err, records: records.length }, 'DynamoDB records');
-      return context.succeed(records.length);
-    });
-  } else {
-    api_request(params, function (err, response) { // get packages from NE API
-      AwsHelper.log.info({ err: err, packages: response.result.length }, 'Package results');
+  api_request(params, function (err, response) { // get packages from NE API
+    AwsHelper.log.info({ err: err, packages: response.result.length }, 'Package results');
 
-      var results = unique_packages(response.result); // one package per hotel
-      AwsHelper.log.info({ packages: results.length }, 'Package uniq');
+    var results = unique_packages(response.result); // one package per hotel
+    AwsHelper.log.info({ packages: results.length }, 'Package uniq');
 
-      var packages = results;
-      var hotel_params = {
-        path: 'hotels',
-        stage: params.stage, // always need the stage (environment e.g: ci/prod)
-        hotelIds: packages.map(function (i) { return i.wvHotelPartId; }).join(',')
-      };
+    var packages = results;
+    var hotel_params = {
+      path: 'hotels',
+      stage: params.stage, // always need the stage (environment e.g: ci/prod)
+      hotelIds: packages.map(function (i) { return i.wvHotelPartId; }).join(',')
+    };
 
-      api_request(hotel_params, function (err, hotel_response) { // get hotel info
-        AwsHelper.log.info({ err: err, hotels: hotel_response.result.length }, 'Hotel results');
-        var records = mapper.map_ne_result_to_graphql(packages, hotel_response.result);
-        CACHE[path] = records; // save this exact search path in the CACHEd results
-        batch_insert(stage, bucketId, records, function (err, data) {
-          AwsHelper.log.info({ err: err, records: records.length }, 'DynamoDB records');
-            // during dev we write results to disk for debug - remove these lines in prod.
-            // require('fs').writeFileSync(__dirname + '/test/sample_results/results.json',
-            //   JSON.stringify(records, null, 2));
-          return context.succeed(records.length);
-        });
+    api_request(hotel_params, function (err, hotel_response) { // get hotel info
+      AwsHelper.log.info({ err: err, hotels: hotel_response.result.length }, 'Hotel results');
+      var records = mapper.map_ne_result_to_graphql(packages, hotel_response.result);
+      batch_insert(stage, bucketId, records, function (err, data) {
+        AwsHelper.log.info({ err: err, records: records.length }, 'DynamoDB records');
+          // during dev we write results to disk for debug - remove these lines in prod.
+          // require('fs').writeFileSync(__dirname + '/test/sample_results/results.json',
+          //   JSON.stringify(records, null, 2));
+        return context.succeed(records.length);
       });
     });
-  }
+  });
+  // }
 };
