694794a192d1bc081e01b2e5be541130de4c06b0
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-17 13:12:21 +0100

    removes obsolete test from test/index.test.js as per discussion with @tomgco in https://github.com/numo-labs/lambda-ne-classic-package-provider/pull/47#discussion_r63322566

--- test/index.test.js
+++ test/index.test.js
@@ -6,42 +6,6 @@ var CONTEXT = {
   functionVersion: '1',
   invokedFunctionArn: 'arn:aws:lambda:eu-west-1:655240711487:function:LambdaTest:$LATEST'
 };
-// var EVENT = {
-//   'Records': [
-//     {
-//       'Sns': {
-//         'Message': '{\"data\":{\"context\":{\"market\":\"dk\",\"language\":\"en-EN\",\"userId\":\"8aeb3560-0b92-11e6-9605-eb677966096c\"},\"query\":{\"passengers\":[{\"birthday\":\"1986-07-14\"},{\"birthday\":\"1986-07-14\"},{\"birthday\":\"2012-07-14\"},{\"birthday\":\"2015-07-14\"}],\"hotels\":[\"hotel:NE.wvHotelPartId.186356\",\"hotel:NE.wvHotelPartId.197915\",\"hotel:NE.wvHotelPartId.197941\"]}},\"id\":\"12345\"}'
-//       }
-//     }
-//   ]
-// };
-//
-// console.log(' - - - - - - - - - - - - - - - - - - - - TEST SNS MESSAGE:');
-// console.log(JSON.stringify(EVENT, null, 2));
-// console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
-//
-// describe('Search request handler ', function () {
-//   it('invoke the lambda function handler $LATEST', function (done) {
-//     CONTEXT.succeed = function () {
-//       // console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
-//       // console.log(arguments); // the argument to context.succeed
-//       assert(arguments[0] > 0);
-//       done();
-//     };
-//     handler(EVENT, CONTEXT);
-//   });
-//
-//   it('invoke the lambda function handler CI', function (done) {
-//     CONTEXT.invokedFunctionArn = 'arn:aws:lambda:eu-west-1:655240711487:function:LambdaTest:$LATEST';
-//     CONTEXT.succeed = function () {
-//       // console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
-//       // console.log(JSON.stringify(arguments[0], null, 2)); // the argument to context.succeed
-//       assert(arguments[0] > 0);
-//       done();
-//     };
-//     handler(EVENT, CONTEXT);
-//   });
-// });
 
 var real_event = require('./fixtures/sample_sns_event.json');
 var COUNT = 0;
@@ -63,7 +27,7 @@ describe('Thailand End-to-End Test', function () {
     CONTEXT.succeed = function () {
       // console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
       // console.log(JSON.stringify(arguments[0], null, 2)); // the argument to context.succeed
-      assert.equal(COUNT, arguments[0]);
+      assert(arguments[0] > 0);
       console.log('Took:', Date.now() - start, 'ms');
       done();
     };
