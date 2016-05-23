6e93cd026981b074ce8c5013d93456768f4440fa
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-23 17:48:12 +0100

    [WiP] initial implementation. still need to re-factor index.js ...

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var AwsHelper = require('aws-lambda-helper');
 var api_request = require('./lib/api_request');
-var mapper = require('./lib/result_mapper');
 var batch_insert = require('./lib/dynamo_insert');
 var parse_sns = require('./lib/parse_sns');
 /**
@@ -20,24 +19,31 @@ exports.handler = function (event, context) {
   delete params.id;         // don't send bucketId to NE api
 
   api_request(params, function (err, response) { // get packages from NE API
-    AwsHelper.log.info({ err: err, packages: response.result.length }, 'Package results');
-
-    var packages = response.result;
-    if (packages.length === 0) {
+    if (err || response.result.length === 0) {\\
       AwsHelper.log.info('No packages found');
       return context.fail('No packages found');
     }
+    AwsHelper.log.info({ err: err, packages: response.result.length }, 'Package results');
+    var packages = response.result;
+
     var hotel_params = {
       path: 'hotels',
       stage: params.stage, // always need the stage (environment e.g: ci/prod)
       hotelIds: packages.map(function (i) { return i.wvHotelPartId; }).join(',')
     };
-    // console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
-    // console.log(hotel_params.hotelIds);
-    // console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
+
     api_request(hotel_params, function (err, hotel_response) { // get hotel info
       AwsHelper.log.info({ err: err, hotels: hotel_response.result.length }, 'Hotel results');
       var records = mapper.map_ne_result_to_graphql(packages, hotel_response.result);
+
+      var message = JSON.parse(event.Records[0].Sns.Message);
+      var body = {
+        id: message.data.context.connectionId,
+        searchId: message.id,
+        userId: message.data.context.userId,
+        items: records
+      }
+
       batch_insert(stage, bucketId, records, function (err, data) {
         AwsHelper.log.info({ err: err, records: records.length }, 'DynamoDB records');
           // during dev we write results to disk for debug - keep commented in prod.
