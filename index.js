d98c391838dff61b7e875450d376b8dd9d06fc36
hveldstra <h@veldstra.org>
2016-06-03 16:42:28 +0100

    Add more logging for debugging

--- index.js
+++ index.js
@@ -11,12 +11,27 @@ var batch_insert = require('./lib/dynamo_insert');
  */
 exports.handler = function (event, context, callback) {
   context.callbackWaitsForEmptyEventLoop = false;
+
   AwsHelper.init(context, event); // to extract the version (ci/prod) from Arn
   AwsHelper.Logger('lambda-ne-classic-package-provider');
   AwsHelper.log.info({ event: event }, 'Received event'); // debug sns
   var params = parse_sns(event.Records[0].Sns.Message);
   var stage = AwsHelper.version; // get environment e.g: ci or prod
   params.stage = stage = (stage === '$LATEST' || !stage) ? 'ci' : stage;
+
+  var resultsReturned = 0;
+  if (params.hotelIds && params.hotelIds.length > 0) {
+    var hids = params.hotelIds.split(',');
+    AwsHelper.log.info({ hotels: hids.length },
+                       'Number of Hotel IDs to get packages for');
+
+    setInterval(function () {
+      if (context.getRemainingTimeInMillis() < 1000) {
+        AwsHelper.log.info({ count: hids - resultsReturned },
+                           'Hotels still remaining just before we time out');
+      }
+    }, 500).unref();
+  }
 
   api_request(params, function (err, response) { // get packages from NE API
     if (err || !response.result || response.result.length === 0) {
@@ -37,6 +52,7 @@ exports.handler = function (event, context, callback) {
       return callback(err, response.result.length);
     });
   }).on('result', function (body) {
+    resultsReturned++;
     // AwsHelper.pushResultToClient requires that each item has a url defined
     body.items = body.items.map(function (item) { // so update the list of items
       item.url = params.searchId + '/' + item.id; // to include an item.url
